982277f35d72cf76fa254356645411d38874786d
Cameron <cameroncook6@yahoo.com>
2016-10-30 15:31:35 -0400

    Added the Login Screen

---
+++ finalProject/finalProject/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace finalProject
+{
+    partial class formLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.txtBoxUser = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtBoxPass = new System.Windows.Forms.TextBox();
+            this.grpBoxLogin = new System.Windows.Forms.GroupBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.grpBoxLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.Location = new System.Drawing.Point(8, 20);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(91, 37);
+            this.lblUserName.TabIndex = 0;
+            this.lblUserName.Text = "UserName : ";
+            this.lblUserName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblUserName.Click += new System.EventHandler(this.lblUserName_Click);
+            // 
+            // txtBoxUser
+            // 
+            this.txtBoxUser.Location = new System.Drawing.Point(102, 29);
+            this.txtBoxUser.Name = "txtBoxUser";
+            this.txtBoxUser.Size = new System.Drawing.Size(153, 20);
+            this.txtBoxUser.TabIndex = 1;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.Location = new System.Drawing.Point(6, 57);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(91, 37);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password :";
+            this.lblPassword.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtBoxPass
+            // 
+            this.txtBoxPass.Location = new System.Drawing.Point(102, 66);
+            this.txtBoxPass.Name = "txtBoxPass";
+            this.txtBoxPass.Size = new System.Drawing.Size(153, 20);
+            this.txtBoxPass.TabIndex = 3;
+            // 
+            // grpBoxLogin
+            // 
+            this.grpBoxLogin.Controls.Add(this.btnSubmit);
+            this.grpBoxLogin.Controls.Add(this.txtBoxPass);
+            this.grpBoxLogin.Controls.Add(this.lblPassword);
+            this.grpBoxLogin.Controls.Add(this.txtBoxUser);
+            this.grpBoxLogin.Controls.Add(this.lblUserName);
+            this.grpBoxLogin.Location = new System.Drawing.Point(4, 29);
+            this.grpBoxLogin.Name = "grpBoxLogin";
+            this.grpBoxLogin.Size = new System.Drawing.Size(268, 171);
+            this.grpBoxLogin.TabIndex = 4;
+            this.grpBoxLogin.TabStop = false;
+            this.grpBoxLogin.Text = "Login";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(102, 128);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // formLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 219);
+            this.Controls.Add(this.grpBoxLogin);
+            this.Name = "formLogin";
+            this.Text = "Login";
+            this.grpBoxLogin.ResumeLayout(false);
+            this.grpBoxLogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.TextBox txtBoxUser;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtBoxPass;
+        private System.Windows.Forms.GroupBox grpBoxLogin;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
